f272927379b5ca7016f8bdca4e882228158632f7
EmanoelleCassia <164902974+EmanoelleCassia@users.noreply.github.com>
2024-11-14 19:51:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'Eu sou Emanoelle de Cassia, tenho 16 anos, estudo no colégio Morski em Pinhão e estou cursando o 2º ano nas Exatas',
+)
+
+criaCartao(
+    'Minha Família é?',
+    '',
+    'Minha família é composta por 3 pessoas, sendo eu, minha irmã mais nova e minha mãe',
+)
+
+criaCartao(
+    'Onde moro?',
+    '',
+    '-25,6581787, -51,6667455',
+)
+
+criaCartao(
+    'Onde estudo?',
+    '',
+    'Fundada em 12/04/1989, a empresa Mário Evaldo Morski possui uma trajetória de 35 anos',
+)
+
+criaCartao(
+    'Minha turma',
+    '',
+    'Minha turma é formada por alunos que se interessam em aprender e absorver o máximo de conhecimento que conseguirem, mas também possui alunos desinteressados',
+)
+
+criaCartao(
+    'Faculdade',
+    '',
+    'Tenho interesse em Desenvolvimento de Jogos, Engenharia ou Desenvolvimento de Softwares e Engenharia da Computação, mas ainda não me decidi sobre qual',
+)
+
+criaCartao(
+    'Minha cidade',
+    '',
+    'Minha cidade é pequena e desinteressante, não tem muitas opções de passeios legais',
+)
+
+criaCartao(
+    'Meu sonho',
+    '',
+    'Meu sonho é conhecer o Japão',
+)
